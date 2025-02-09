b61d28946534689982264905f081879a3439b807
ValentinoFilipetto <valefilipetto@gmail.com>
2025-02-09 12:12:11 +0100

    feat: create and draw two-dimensional grid

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
 from cell import Cell
+from maze import Maze
 from window import Line, Point, Window
 
 
 def main():
     point_zero = Point(0, 0)
-    point_one = Point(50, 100)
-    point_two = Point(100, 50)
-    point_three = Point(150, 100)
-    point_four = Point(200, 50)
+
+    # top-left corner and bottom-right corner to make one square
+    # point_one = Point(100, 200)
+    # point_two = Point(200, 100)
+
+    # point_three = Point(150, 100)
+    # point_four = Point(200, 50)
+
     win = Window(800, 600)
-    first_cell = Cell(win)
-    second_cell = Cell(win)
+    # first_cell = Cell(win)
+    # second_cell = Cell(win)
 
     # my_line = Line(point_zero, point_four)
     # win.draw_line(my_line, "red")
 
-    first_cell.draw(point_one.x, point_two.x, point_two.y, point_one.y)
-    second_cell.draw(point_three.x, point_four.x, point_four.y, point_three.y)
+    # first_cell.draw(point_one.x, point_two.x, point_two.y, point_one.y)
+    # second_cell.draw(point_three.x, point_four.x, point_four.y, point_three.y)
 
-    first_cell.draw_move(second_cell)
+    # first_cell.draw_move(second_cell)
+
+    maze = Maze(50, 50, 10, 10, 25, 25, win)
+    maze._create_cells()
 
     win.wait_for_close()
 
---
+++ maze.py
@@ -0,0 +1,48 @@
+import time
+from cell import Cell
+from window import Point
+
+
+class Maze:
+    def __init__(
+        self,
+        x1,
+        y1,
+        num_rows,
+        num_cols,
+        cell_size_x,
+        cell_size_y,
+        win,
+    ):
+        self.x1 = x1
+        self.y1 = y1
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        self.cell_size_x = cell_size_x
+        self.cell_size_y = cell_size_y
+        self.win = win
+        self._cells = []
+
+    def _create_cells(self):
+        self._cells = [[] for col in range(self.num_rows)]
+        for row_idx, i in enumerate(range(self.num_rows)):
+            for col_idx, j in enumerate(range(self.num_cols)):
+                self._draw_cell(i, j)
+
+    def _draw_cell(self, i, j):
+        point_one = Point(
+            self.x1 + (i * self.cell_size_x), self.y1 + (j * self.cell_size_y)
+        )
+        point_two = Point(
+            self.x1 + ((i + 1) * self.cell_size_x),
+            self.y1 + ((j + 1) * self.cell_size_y),
+        )
+
+        cell = Cell(self.win)
+        self._cells[i].append(cell)
+        cell.draw(point_one.x, point_two.x, point_two.y, point_one.y)
+        self._animate()
+
+    def _animate(self):
+        self.win.redraw()
+        time.sleep(0.03)
